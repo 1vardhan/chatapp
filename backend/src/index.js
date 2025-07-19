f53a85a3073d2da8bc5109bfafa22489140766a2
1vardhan <sunkara.vdn@gmail.com>
2025-07-19 11:55:04 +0530

    Fix path typo in production route

--- backend/src/index.js
+++ backend/src/index.js
@@ -31,13 +31,14 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages",messageRoutes);
 
-if(process.env.NODE_ENV==="production"){
-  app.use(express.static(path.join(_dirname,"../frontend/dist")));
+if(process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(_dirname, "../frontend/dist")));
 
-  app.get("*",(req,res)=>{
-    res.sendFile(path.join(_dirname,"..frontend","dist","index.html"));
-  })
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(_dirname, "../frontend", "dist", "index.html"));
+  });
 }
+
 
 // Root route to check if server is running
 app.get("/", (req, res) => {
